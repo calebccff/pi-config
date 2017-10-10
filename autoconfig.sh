f564d6312839a67c48642fa253eeb05bf48738a5
calebccff <calebccff@gmail.com>
2017-10-10 14:49:35 +0100

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-echo "Updating..."
-sudo apt-get update -y
-
-echo "Installing desktop environment"
-sudo apt-get install -y --no-install-recommends xserver-xorg xinit
-sudo apt-get install -y xorg openbox xdm pcmanfm lxappearance lxpanel gmrun gnome-terminal
-
-echo "Install apache2"
-sudo apt-get install -y apache2 apache2-doc apache2-utils
-echo "Install MYSQL"
-sudo apt-get install -y mysql-server
-
 echo "Making changes"
 mkdir ~/.config
 mkdir ~/.config/openbox
@@ -19,12 +5,10 @@ touch ~/.config/openbox/autostart
 echo "lxpanel &" > ~/.config/openbox/autostart
 
 echo "Overclocking...."
-sudo su
+sudo chown pi /boot/config.txt
 echo "arm_freq=1000" >> /boot/config.txt
 echo "core_freq=500" >> /boot/config.txt
 echo "sdram_freq=600" >> /boot/config.txt
 echo "over_voltage=6" >> /boot/config.txt
-exit
-
-echo "Rebooting..."
-sudo init 6
+echo "Done!"
+cat /boot/config.txt
